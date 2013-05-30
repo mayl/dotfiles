22ad4617a9d7b039830e2bb9a25c49d8ed23583b
larry <larry@larry-ThinkPad-X220.(none)>
2013-05-29 22:48:38 -0400

    removed peat to re-add as a submodule

--- makesymlinks.py
+++ makesymlinks.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 
 from sh import git
+from sh import mkdir
+from sh import cd
+from sh import ln
 
-dir = "~/dotfiles"
-olddir = "~/dotfiles_old"
+print "pulling git submodule content"
+git("submodule", "init")
+git("submodule", "update")
+
+newdir = "/dotfiles"
+olddir = "/dotfiles_old"
 
 files = ["bashrc", "vimrc",  "vim", "Xresources", "/config/fish/config.fish"]
-
-for word in files:
-	print word
+folders = ["/config/fish, /fonts"]
 
 
+print "Creating " + olddir + " for backup of any existing dotfiles..."
+mkdir("-p", "$HOME"+olddir)
+print "done"
+
+print "Changing to " + newdir + "..."
+cd("$HOME")
+print "done"
+
+print "copying backups of old files to <" + olddir + ">and symlinking new dotfiles in....\n"
+for f in files:
+	print f + "...",
+	print "copied...",
+	print "linked...",
+	print " done"
+	pass
