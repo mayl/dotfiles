c19b25a0e687ba330013252c50ac09572729e632
Larry May <larrymayster@gmail.com>
2016-06-20 20:57:27 -0400

    moving dotfiles to gnu stow compatible format

---
+++ omf/init.fish
@@ -0,0 +1,73 @@
+#GOPATHS
+set -x GOROOT $HOME/go 
+set -x TERM $HOME/go 
+set -x GOPATH $HOME/code/go
+set -x PATH $PATH $GOROOT/bin
+set -x PATH $PATH $GOPATH/bin
+alias gofmt "go fmt -w"
+
+#TERM variable
+set -x TERM screen-256color
+
+#stats godoc server up in background and displays a reminder
+function godoc_web
+	echo "open localhost:6060"
+	godoc -http=:6060 &
+end
+
+
+#use trash instead of rm
+alias rm "trash"
+
+#setup t
+alias t "python ~/dotfiles/t/t.py --task-dir ~/Dropbox/tasks --list tasks"
+
+#mindmaps
+function mm
+	freemind ~/Dropbox/mindmaps/Larry.mm &
+end
+
+#redshift
+function rs
+	gtk-redshift -l 38.731950:-77.182701 &
+end
+
+#setup peat
+set -x PATH $PATH ~/dotfiles/peat
+
+#arduino Burner
+alias arduinoburner "python ~/code/python/ArduinoBurner/ArduinoBurner.py"
+
+#unison
+alias unison "sudo unison-gtk"
+
+#ack
+alias ack "ack-grep"
+
+#camlistore
+set -x PATH $PATH ~/camlistore-0.6/bin/
+set -x PATH $PATH ~/camlistore-0.7/bin
+
+#antares
+set -x ANTARES_INSTALL_DIR ~/code/c/antares
+
+#i3 mute
+function muteToggle
+	if pacmd list-sinks | grep "muted: yes"
+		pactl set-sink-mute 1 0
+		echo "Muted"
+	else
+		pactl set-sink-mute 1 1
+		echo "Not Muted"
+	end
+end
+
+#scanner
+set -x SANE_DEFAULT_DEVICE "brother4:bus9;dev1"
+
+#ipython notebook
+alias ipyNotebook "ipython notebook --notebook-dir=~/Dropbox/Projects/ipythonNotebooks &"
+
+#rust
+set -x PATH $PATH ~/.cargo/bin/
+set -gx RUST_SRC_PATH /usr/local/src/rust/src/rustc-1.9.0/src/
