c8f1299295fa6b32218d87e40e53b67017ce2b85
LarryM <larrymayster@gmail.com>
2019-10-27 20:34:18 +0900

    improve fzf and fisher compatibility

--- fish/omf/init.fish
+++ fish/omf/init.fish
@@ -29,5 +29,15 @@ if test -d ~/.cargo/
     set -xg PATH $PATH ~/.cargo/bin
 end
 
+if test -d ~/.fzf/ 
+    set -xg PATH $PATH ~/.fzf/bin
+end
+
 #get pip install -U binaries into path
 set -xg PATH $PATH ~/.local/bin
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
